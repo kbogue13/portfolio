b99dde98ddb1e7011e52a9be769af1731f09d3e4
kbogue13 <kbogue13@uw.edu>
2017-10-15 19:22:32 -0700

    email test

--- contact.php
+++ contact.php
@@ -1,7 +1,19 @@
 <?php
-if($_POST["message"]) {
-    mail("kbogue13@uw.edu", "Form to email message", $_POST["message"], "From: an@email.address");
+
+if($_POST["submit"]) {
+    $recipient="your@email.address";
+    $subject="Form to email message";
+    $sender=$_POST["sender"];
+    $senderEmail=$_POST["senderEmail"];
+    $message=$_POST["message"];
+
+    $mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
+
+    mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
+
+    $thankYou="<p>Thank you! Your message has been sent.</p>";
 }
+
 ?>
 <!DOCTYPE HTML>
 <!--
